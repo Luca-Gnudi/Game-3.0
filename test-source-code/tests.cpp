f75a02bfbc31ba1ccbf145688cc7829d4b0166ba
Matthew Bouwer <matt.ibouwer@gmail.com>
2023-09-14 18:12:37 +0200

    Missile Movement Test

--- test-source-code/tests.cpp
+++ test-source-code/tests.cpp
@@ -20,3 +20,29 @@ TEST_CASE("Lander starts to move towards its target") {
     CHECK(lander.getPosition().x > initialPosition.x);
     CHECK(lander.getPosition().y > initialPosition.y);
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                                          Missile                                              //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+TEST_CASE("Missile moves towards target position") {
+    // Create a target position (space ship's position)
+    sf::Vector2f targetPosition(100.0f, 200.0f);
+
+    // Create a missile at some initial position
+    sf::Vector2f initialPosition(50.0f, 50.0f);
+    Missile missile(initialPosition, targetPosition);
+
+    // Set a fixed deltaTime for testing
+    float deltaTime = 10.0f;
+
+    // Update the missile's position based on deltaTime
+    missile.updatePosition(deltaTime);
+
+    // Check if the missile has moved towards the target position
+    sf::Vector2f newPosition = missile.getPosition();
+
+    // Assert that the missile's new position is closer to the target position
+    CHECK(newPosition.x > initialPosition.x);
+    CHECK(newPosition.y > initialPosition.y);
+}
