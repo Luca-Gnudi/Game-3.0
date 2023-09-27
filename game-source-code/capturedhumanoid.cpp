4cc040384271c182c34c0048210600cea613ce80
Matthew Bouwer <matt.ibouwer@gmail.com>
2023-09-27 16:45:53 +0200

    Captured Humanoid

---
+++ game-source-code/capturedhumanoid.cpp
@@ -0,0 +1,47 @@
+#include "capturedhumanoid.h"
+#include <iostream>
+
+CapturedHumanoid::CapturedHumanoid()  {
+    capturedhumanoidTexture = new sf::Texture;
+    if (!capturedhumanoidTexture->loadFromFile("resources/assets/CapturedHumanoid.png")) {
+        std::cout << "Could not load captured humanoid image file";
+    }
+    capturedhumanoidSprite.setTexture(*capturedhumanoidTexture);
+    capturedhumanoidSprite.setScale(1.0f, 1.0f); // Adjust the scale as needed
+}
+
+void CapturedHumanoid::setPosition(const sf::Vector2f& position) {
+    capturedhumanoidSprite.setPosition(position);
+}
+
+void CapturedHumanoid::updatePosition(float deltaTime) {
+    // Adjust the speed as needed
+    float moveSpeed = 100.0f;
+    capturedhumanoidSprite.move(0.0f, -moveSpeed * deltaTime);
+}
+
+sf::FloatRect CapturedHumanoid::getHitBox() {
+    return capturedhumanoidSprite.getGlobalBounds();
+}
+
+void CapturedHumanoid::draw(sf::RenderWindow& window) {
+    if (active) {
+        window.draw(capturedhumanoidSprite);
+    }
+}
+
+void CapturedHumanoid::setActive(bool isActive) {
+    active = isActive;
+}
+
+bool CapturedHumanoid::isActive() const {
+    return active;
+}
+
+sf::Vector2f CapturedHumanoid::getPosition() const {
+    return capturedhumanoidSprite.getPosition();
+}
+
+float CapturedHumanoid::getHeight() const {
+    return capturedhumanoidSprite.getGlobalBounds().height;
+}
