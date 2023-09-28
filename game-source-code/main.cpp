ee75b9713f326895f314e721942b8b04078141e0
Matthew Bouwer <matt.ibouwer@gmail.com>
2023-09-28 12:09:37 +0200

    Main 1

--- game-source-code/main.cpp
+++ game-source-code/main.cpp
@@ -1,6 +1,19 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 #include "Bullet.h"
 #include "lander.h"
+#include "explosion.h"
+#include "humanoid.h"
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include "Bullet.h"
+#include "lander.h"
+#include "SpaceShip.h"
+
+// Collision detection logic
+bool isCollision(const sf::FloatRect& rect1, const sf::FloatRect& rect2) {
+    return rect1.intersects(rect2);
+}
 
 int main(){
     const int gameWidth = 1600; //The width of the game screen.
@@ -10,13 +23,31 @@ int main(){
     window.setVerticalSyncEnabled(true);
     window.setFramerateLimit(60);//Helps reduce toll on processing.
     sf::Texture BackgroundTexture;
-    if(!BackgroundTexture.loadFromFile("resources/Space-Background-Image-7.jpg")){
+    if(!BackgroundTexture.loadFromFile("resources/Space-mountain.png")){
         return EXIT_FAILURE;
     }
    
     sf::Sprite Background;
     Background.setTexture(BackgroundTexture);
     Background.setPosition(0,0);
+    //Background.setScale(0.67f,0.67f);
+
+    //Settting up a game view to allow for scrolling.
+    sf::View GameView;
+    GameView.reset(sf::FloatRect(0,0,gameWidth,gameHeight));
+    //GameView.setViewport(sf::FloatRect(0,0,0.6f,0.6f));
+    
+    //Setting up a MiniMap View.
+    sf::View MiniMapView;
+    MiniMapView.reset(sf::FloatRect(0.f,0.f,Background.getGlobalBounds().width,Background.getGlobalBounds().height));
+    MiniMapView.setViewport(sf::FloatRect(0.2f,0.f,0.66f,0.15f));
+
+    //Setting up a higlight square for the minimap. possibly try use the game view.
+    sf::RectangleShape CurrentView(sf::Vector2f(gameWidth,gameHeight));
+    CurrentView.setOrigin(gameWidth/2,gameHeight/2);
+    CurrentView.setFillColor(sf::Color::Transparent);
+    CurrentView.setOutlineThickness(-10);
+    CurrentView.setOutlineColor(sf::Color::Red);
     
     sf::Texture spaceShipTexture;
     if (!spaceShipTexture.loadFromFile("resources/assets/space_ship.png")) {
@@ -24,17 +55,23 @@ int main(){
     }
 
     // Create a sprite for the space ship and set its texture
-    sf::Sprite spaceShip;
-    float scale = 5.5f;
-    spaceShip.setTexture(spaceShipTexture);
-    spaceShip.setScale(scale, scale); // Adjust the scale as needed
+    float scale = 2.5f;
 
     //Create the space ship.
     const int ShipSize = 175;
-    sf::Vector2f spaceShipPosition(gameWidth-ShipSize, gameHeight/2);
-    spaceShip.setPosition(spaceShipPosition);
-    spaceShip.setOrigin(sf::Vector2f(scale,scale));
+    sf::Vector2f spaceShipPosition((Background.getGlobalBounds().width/2)-ShipSize, gameHeight/2);
     const float shipSpeed = 1000.f;
+
+    //Create a hitbox for the space ship
+    sf::FloatRect spaceShipHitBox;
+
+    sf::Clock bulletCooldownTimer;
+    const sf::Time bulletCooldownDuration = sf::seconds(0.5f); // Adjust the cooldown duration as needed
+    bool canShootBullet = true;
+
+    auto landerShot = 0;
+
+    SpaceShip spaceShip(scale, shipSpeed, spaceShipPosition, Background); // Create the spaceShip
 
     // Load the text font
     sf::Font font;
@@ -61,114 +98,4 @@ int main(){
     sf::Clock AITimer;
     const sf::Time AITime = sf::seconds(0.1f);
 
-    sf::Clock clock;
-    auto isPlaying = false;
-
-    sf::Vector2f centerPosition(gameWidth / 2.0f, gameHeight / 2.0f);
-    Lander lander(centerPosition); // Specify the initial position
-
-    auto isleft = true;//bool for the direction that the lazer shoots.
     
-    std::vector<Bullet> bullets;
-    auto bulletSpeed = 40.f;
-    while (window.isOpen()){
-        
-     //bool isLazers = false;//bool to determine if lazers should be drawn.
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Window closed or escape key pressed: exit
-	        if((event.type == sf::Event::Closed) ||
-	            ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Escape))) {
-		    window.close();
-		    break;
-	        }
-
-	    // Enter key pressed: play
-	    if((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Enter)) {
-		if(!isPlaying) {
-		    // (re)start the game
-		    isPlaying = true;
-		    clock.restart();
-
-		    // Reset the position of the paddles and ball
-		    spaceShip.setPosition(spaceShipPosition);
-		}
-	    }
-	}
-       if (isPlaying) {
-            float deltaTime = clock.restart().asSeconds();
-
-            // Moving the space ship vertically and horizontally
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && (spaceShip.getPosition().y > 0)) {
-                spaceShip.move(0.f, -shipSpeed * deltaTime);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && (spaceShip.getPosition().y + spaceShip.getLocalBounds().height < gameHeight-75)) {
-                spaceShip.move(0.f, shipSpeed * deltaTime);
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && (spaceShip.getPosition().x > 25)) {
-                spaceShip.move(-shipSpeed * deltaTime, 0.f);
-                spaceShip.setScale(scale,scale);
-                isleft = true;
-            }
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && (spaceShip.getPosition().x + spaceShip.getLocalBounds().width < gameWidth)) {
-                spaceShip.move(shipSpeed * deltaTime, 0.f);
-                spaceShip.setScale(-scale,scale);
-                isleft = false;
-            }
-             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-            if (isleft){
-                Bullet bullet(spaceShip.getPosition(), -1, bulletSpeed);
-                bullet.setActive(true);
-                bullets.push_back(bullet);
-            }
-            else {
-                Bullet bullet(spaceShip.getPosition(), 1, bulletSpeed);
-                bullet.setActive(true);
-                bullets.push_back(bullet);
-            }
-            }
-}
-
-            for (auto& bullet : bullets) {
-                bullet.update();
-            }
-
-
-         //rendering
-        window.clear();
-        window.draw(Background);
-        if(isPlaying){
-
-            float deltaTime = clock.restart().asSeconds();
-            Bullet::removeInactiveBullets(bullets);
-            window.draw(spaceShip);
-
-            for (auto& bullet : bullets) {
-                bullet.draw(window);
-            }
-        
-           // Call updateMissile to handle missile shooting
-            lander.missileShoot(deltaTime, gameWidth, gameHeight, spaceShip.getPosition());
-            // Update and draw the enemy
-            lander.updatePosition(spaceShip.getPosition(), deltaTime);
-            lander.draw(window);
-            lander.missileDraw(window);
-        }
-        else{
-            // Draw the pause message
-	        window.draw(pauseMessage);
-            window.draw(playInstructions);
-        }
-        
-        window.display();
-
-
-}
- 
-}
-
-
-  
-
-
